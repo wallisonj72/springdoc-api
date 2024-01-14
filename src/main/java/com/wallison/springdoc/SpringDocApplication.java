8178e5b6de81cddf595ae868a732afb4778e78b8
Wallison Júnior <83143385+wallisonj72@users.noreply.github.com>
2024-01-14 16:58:45 -0300

    @OpenAPIDefinition

--- src/main/java/com/wallison/springdoc/SpringDocApplication.java
+++ src/main/java/com/wallison/springdoc/SpringDocApplication.java
@@ -1,13 +1,33 @@
 package com.wallison.springdoc;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "SpringDoc Project",
+                version = "0.1",
+                description = "This is Project for learning",
+                termsOfService = "Terms here",
+                contact = @Contact(
+                        name = "Wallison",
+                        email = "teste@gmail.com"
+                ),
+                license = @License(
+                        name = "Lincense",
+                        url = "www.testespringdoc.com"
+                )
+        )
+)
 @SpringBootApplication
 public class SpringDocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDocApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDocApplication.class, args);
+    }
 
 }
